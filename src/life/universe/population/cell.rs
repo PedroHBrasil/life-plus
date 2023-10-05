712bd31ddc650448f771229913cd740b7e42acee
PedroHBrasil <pedrohbrasil7@gmail.com>
2023-10-05 18:33:13 +0000

    Implemented cell

--- src/life/universe/population/cell.rs
+++ src/life/universe/population/cell.rs
@@ -2,17 +2,50 @@ use crate::util::{Coord, Color};
 
 pub struct Cell {
   pub lives: bool,
-  origin: Coord,
-  size: u16,
-  color: Color,
+  pub origin: Coord,
+  pub size: u16,
+  pub color: Color,
 }
 
 impl Cell {
-  pub fn new() -> Self {
-    unimplemented!()
-  }
-
   pub fn update_life(&mut self, n_alive_neighbors: u8) {
-    unimplemented!()
+    // This line summarizes the original rules found at https://conwaylife.com/
+    self.lives = n_alive_neighbors == 3 || (n_alive_neighbors == 2 && self.lives);
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn update_life() {
+    let mut cell = Cell {
+      lives: false,
+      origin: Coord{x: 0, y: 0},
+      size: 1,
+      color: Color{red: 255, green: 0, blue: 0, alpha: 255},
+    };
+
+    // Tests for initially dead cell
+    for i in 0..=9 {
+      cell.update_life(i);
+      if i == 3 {
+        assert!(cell.lives);
+      } else {
+        assert!(!cell.lives);
+      }
+    }
+
+    // Tests for initially live cell
+    for i in 0..=9 {
+      cell.lives = true;
+      cell.update_life(i);
+      if i == 2 || i == 3 {
+        assert!(cell.lives);
+      } else {
+        assert!(!cell.lives);
+      }
+    }
   }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,11 @@
 pub struct Color {
-  red: u8,
-  green: u8,
-  blue: u8,
-  alpha: u8
+  pub red: u8,
+  pub green: u8,
+  pub blue: u8,
+  pub alpha: u8
 }
 
 pub struct Coord {
-  x: u16,
-  y: u16,
+  pub x: u16,
+  pub y: u16,
 }
